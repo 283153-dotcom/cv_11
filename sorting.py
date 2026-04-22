76e55cdb4793e08be9b30377887c94acfc42a5e6
Hedvika Cempírková <283153@vutbr.cz>
2026-04-22 09:42:59 +0200

    bubble_sort

--- sorting.py
+++ sorting.py
@@ -1,5 +1,4 @@
 import random
-from operator import index
 
 
 def random_numbers(count, low=0, high=100):
@@ -14,16 +13,35 @@ def selection_sort(my_numbers):
         for idx in range(i+1,n):
             if my_numbers_copy[idx]<my_numbers_copy[current_min]:
                 current_min=idx
-        my_numbers_copy[i], my_numbers_copy[current_min] = my_numbers_copy[current_min], my_numbers_copy[i]
+        if current_min != i:
+            my_numbers_copy[i], my_numbers_copy[current_min] = my_numbers_copy[current_min], my_numbers_copy[i]
 
     return my_numbers_copy
 
 
+def bubble_sort(my_numbers):
+    my_numbers_bubble = my_numbers.copy()
+    n=len(my_numbers_bubble)
+    #swtich=1
+
+    for i in range(n-1): #šlo by to taky přes počet výměn, while switch !=0
+        for idx in range(n-1-i):
+            if my_numbers_bubble[idx]>my_numbers_bubble[idx+1]: #switch +=1
+                my_numbers_bubble[idx],my_numbers_bubble[idx+1] = my_numbers_bubble[idx+1], my_numbers_bubble[idx]
+
+    return my_numbers_bubble
+
+
 def main():
-    my_numbers=random_numbers(5)
-    sorted_numbers=selection_sort(my_numbers)
-    print(sorted_numbers)
+    my_numbers=random_numbers(20)
+
+    sorted_numbers_selection=selection_sort(my_numbers)
+    print(sorted_numbers_selection)
+
+    sorted_numbers_bubble=bubble_sort(my_numbers)
+    print(sorted_numbers_bubble)
+
 
 
 if __name__ == "__main__":
-    print(selection_sort([52,45,11,0,25,0,32]))
+    print(main())
